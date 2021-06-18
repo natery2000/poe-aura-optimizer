dac50695b103c22d6ca5feef7d24f7237894e998
Nathan <you@example.com>
2021-06-17 19:28:17 -0500

    Started algorithm, added available gem slots

--- main.js
+++ main.js
@@ -1,37 +1,70 @@
 import { getOptimizedAuras } from "./optimizer.js";
 import { auras } from "./auras.js";
+import { equipmentLimits } from "./equimentLimits.js";
+
+function getOptions() {
+    var options = {};
+    options.auras = [];
+    auras.forEach(function(aura) {
+        if (document.getElementById(aura.name).checked) {options.auras.push(aura);}
+    });
+    options.head = 2;
+    return options;
+}
 
 function optimize() {
-    var optimizedAuras = getOptimizedAuras({auras: auras});
+    var optimizedAuras = getOptimizedAuras(getOptions());
     console.log(optimizedAuras);
 };
 
 var content = document.getElementById("content");
 
+var auraDiv = document.createElement("div");
+content.appendChild(auraDiv);
 auras.forEach(function(aura) {
     var input = document.createElement("input");
     input.type = "checkbox";
     input.id = aura.name;
     input.value = aura.name;
-    content.appendChild(input);
+    auraDiv.appendChild(input);
 
     var label = document.createElement("label");
     label.for = aura.name;
     label.innerHTML = aura.name;
-    content.appendChild(label);
+    auraDiv.appendChild(label);
 
     var br = document.createElement("br");
-    content.appendChild(br);
+    auraDiv.appendChild(br);
 });
 
+var equipmentDiv = document.createElement("div");
+content.appendChild(equipmentDiv);
+equipmentLimits.forEach(function(equipmentLimit) {
+    var select = document.createElement("select");
+    select.name = equipmentLimit.name;
+    select.id = equipmentLimit.name;
+    for(var i = 0; i < equipmentLimit.limit; i++) {
+        var item = document.createElement("option");
+        item.value = i + 1;
+        item.innerHTML = i + 1;
+        select.appendChild(item);
+    }
+    equipmentDiv.appendChild(select);
+
+    var label = document.createElement("label");
+    label.for = equipmentLimit.name;
+    label.innerHTML = equipmentLimit.name;
+    equipmentDiv.appendChild(label);
+
+    var br = document.createElement("br");
+    equipmentDiv.appendChild(br);
+
+    select.selectedIndex = equipmentLimit.limit - 1;
+});
+
+var submitDiv = document.createElement("div");
+content.appendChild(submitDiv);
 var submit = document.createElement("button");
 submit.innerHTML = "Optimize";
 submit.onclick = optimize;
-content.appendChild(submit);
-
-        // <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-        // <label for="vehicle1"> I have a bike</label><br>
-        // <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-        // <label for="vehicle2"> I have a car</label><br>
-        // <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
-        // <label for="vehicle3"> I have a boat</label><br></br>
+submitDiv.appendChild(submit);
--- optimizer.js
+++ optimizer.js
@@ -1,5 +1,19 @@
 export function getOptimizedAuras(options) {
+    var headCombinations = combinations(options.auras, options.head);
+
     return {
-        head: [["Grace"],options.auras.map(function(aura) { return aura.name }).concat(["Enlighten(4)"])]
+        head: [headCombinations]
     };
+}
+
+function combinations(list, length) {
+    var set = [],
+        listSize = list.length,
+        combinationsCount = (1 << listSize);
+
+    for (var i = 1; i < combinationsCount; i++, set.push(combination))
+        for (var j=0, combination = []; j<listSize; j++)
+            if (i & (1 << j))
+                combination.push(list[j]);
+    return set.filter(function(auraSet) {return auraSet.length <= length});
 }
